500d2385297f8503c8100ad0efa3195c411d781d
Dir-A <g2576612205@gmail.com>
2024-05-18 10:24:58 +0800

    change ZxDefine

--- include/Zut/ZxDefine.h
+++ include/Zut/ZxDefine.h
@@ -7,41 +7,25 @@
 
 namespace Zqf::Zut
 {
+	constexpr size_t ZUT_MAX_PATH = MAX_PATH;
 
-using SysID = HANDLE;
-
-constexpr size_t ZUT_MAX_PATH = MAX_PATH;
-
-enum class MoveWay
-{
-    Beg = FILE_BEGIN,
-    Cur = FILE_CURRENT,
-    End = FILE_END,
-};
-
-using WalkData_t = WIN32_FIND_DATAW;
-
-enum class STDIOType
-{
-    Write = static_cast<int>(STD_OUTPUT_HANDLE),
-    Read = static_cast<int>(STD_INPUT_HANDLE),
-    Error = static_cast<int>(STD_ERROR_HANDLE),
-};
-
+	using SysID = Wx32::SysID;
+	using CodePage = Wx32::CodePage;
+	using MoveWay = Wx32::MoveWayEnum;
+	using STDHandleType = Wx32::STDHandleEnum;
+	using MbcsStr_t = Wx32::MbcsStr_t;
+	using WideStr_t = Wx32::WideStr_t;
+	using WalkData_t = WIN32_FIND_DATAW;
 } // namespace Zqf::Zut
 
 #endif // ZUT_WIN32
 
 namespace Zqf::Zut
 {
-using MbcsStr_t = Wx32::MbcsStr_t;
-using WideStr_t = Wx32::WideStr_t;
-using CodePage = Wx32::CodePage;
-
-enum class OpenMod
-{
-    ReadSafe,
-    WriteSafe,
-    WriteForce
-};
+	enum class OpenMod
+	{
+		ReadSafe,
+		WriteSafe,
+		WriteForce
+	};
 } // namespace Zqf::Zut
